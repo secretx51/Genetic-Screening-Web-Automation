397caaa80217d53d2ed79d011fd98c2c9862dd25
secretx51 <trent.neilson99@gmail.com>
2023-07-20 14:17:42 +1000

    Added ArgParser and ability to work with different cancers

--- Tide/tideFormat.py
+++ Tide/tideFormat.py
@@ -2,19 +2,31 @@ import pandas as pd
 import numpy as np
 
 class FilerTide():
-    def __init__(self, downloads: str, gene: str, exclusion: bool) -> None:
+    def __init__(self, downloads: str, gene: str, exclusion: bool, cancers: list[str]) -> None:
         self._downloads = downloads
         self._gene = gene
         self._exclusion = exclusion
+        self._cancers = cancers
 
     def _filterRows(self, df):
-        ovarian_df = df[df.iloc[:, 1] == "Ovarian"]
-        ovary_df = df[df.iloc[:, 1] == "Ovary"]
-        return pd.concat([ovarian_df, ovary_df])
+        final_dfs = []
+        for cancer in self._cancers:
+            filter_df = df[df.iloc[:, 1] == cancer]
+            final_dfs.append(filter_df)
+        return pd.concat(final_dfs)
     
+    def _mergeCancerSubtype(self, df):
+        def mergeNonNaRow(row):
+            if pd.notna(row['Subtype']):
+                    return row['Cohort'] + ' ' + row['Subtype']
+            return row['Cohort']
+        
+        df['MergedColumn'] = df.apply(mergeNonNaRow, axis=1)
+        return df
+        
     def _filterColumns(self, df):
         # List of column indices to remove
-        columns_to_remove = [1, 2, 3, 4, 6, 7, 8]
+        columns_to_remove = [0, 1, 2, 3, 4, 6, 7, 8]
         df = df.drop(df.columns[columns_to_remove], axis=1)
         df.reset_index(drop=True, inplace=True)
         df['Gene'] = self._gene
@@ -22,11 +34,11 @@ class FilerTide():
     
     def _tryAddExclusion(self, df):
         try:
-            if self._exclusion:
+            if not self._exclusion: #If exclusion false
                 raise EnvironmentError("Exclusion data not wanted.")
             exclusion_df = pd.read_csv(f"{self._downloads}/{self._gene}_exclusion.csv")
-            return_df = pd.concat([df, exclusion_df], axis=1, ignore_index=True)
-            return return_df
+            return_df = pd.concat([df, exclusion_df], axis=1, ignore_index=False)
+            return return_df #Remove left over labels
         except FileNotFoundError as error:
             print(f"Cannot Find Exclusion File \n {error}")
             return df
@@ -36,51 +48,84 @@ class FilerTide():
     def filterData(self):
         df = pd.read_csv(f"{self._downloads}/{self._gene}.csv")
         rows_df = self._filterRows(df)
-        column_df = self._filterColumns(rows_df)
+        named_df = self._mergeCancerSubtype(rows_df)
+        column_df = self._filterColumns(named_df)
         return self._tryAddExclusion(column_df)
 
 class FormatTide():
     def __init__(self, filename, columns, exclusion: bool) -> None:
         self._filename = filename
-        self._columns = columns
         self._exclusion = exclusion
-        self.Tcga1 = True
         self.genes = []
         self.complete_rows = []
-        self.current_row = np.zeros(len(columns))
+        self.Tcga1 = True
+        self._columns = self._prepColumns(columns)
+        self.current_row = np.zeros(len(columns) + 1) # +1 for TCGA 1 and 2
+
+    def _prepColumns(self, columns: list[str]):
+        columns.insert(0, "Gene")
+        if self._exclusion:
+            columns.extend(['CAF FAP', 'MDSC', 'TAM M2'])
+        return columns
 
     def _sortColumns(self, df):
-        og_columns = ["Cohort", "T Dysfunction", "Gene", "Condition", "Z score"]
-        if len(df.columns) != len(og_columns):
-            difference = len(df.columns) - len(og_columns)
+        og_columns = ["T Dysfunction", "Cohort", "Gene", "Condition", "Z score"]
+        difference = abs(len(df.columns) - len(og_columns))
+        if len(df.columns) < len(og_columns):
+            for missing in range(difference):
+                df[missing] = ''
+        elif len(df.columns) > len(og_columns):
             for missing in range(difference):
                 og_columns.append(str(missing))
         df.columns = og_columns
         return df.sort_values('Gene')
     
     def _resetCurretRow(self, current_gene):
-        if len(self.genes) != 0:
-            if current_gene != self.genes[-1]:
+        if len(self.genes) != 0 and current_gene != self.genes[-1]:
                 append_row = self.current_row.tolist()
                 append_row[0] = self.genes[-1]
                 self.complete_rows.append(append_row)
-                self.current_row = np.zeros(len(self._columns))
-    
+                self.current_row = np.zeros(len(self._columns) + 1) # +1 for TCGA 1 and 2
+
+    def addTCGAcols(self):
+        try:
+            self._columns.index("TCGA1")
+        except ValueError:
+            #Don't need try called in if block for TCGA
+            index = self._columns.index('TCGA') 
+            self._columns.pop(index)
+            self._columns.insert(index, 'TCGA1')
+            self._columns.insert(index + 1, 'TCGA2')
+
     def _tcgaFlip(self, cohort):
         if cohort == 'TCGA':
+            self.addTCGAcols()
             if self.Tcga1:
                 cohort = 'TCGA1'
                 self.Tcga1 = False
             else:
                 cohort = 'TCGA2'
                 self.Tcga1 = True
+        return cohort
 
     def _getValues(self, cohort, row):
         for index, column in enumerate(self._columns):
-            if cohort == column:
+            if cohort.split()[0] == column: #Split the whitespace to remove subtype
                 self.current_row[index] = row['T Dysfunction'] 
             elif self._exclusion and row['Condition'] == column:
                 self.current_row[index] = row['Z score']
+
+    def _outputDf(self):
+        #Drop the difference from the right for every row in array
+        for i, row in enumerate(self.complete_rows): #Index each array in list
+            #If len of row greater than column length
+            if len(self.complete_rows[i]) > len(self._columns): 
+                #Go into row and take up to the length of wanted row
+                self.complete_rows[i] = self.complete_rows[i][:len(self._columns)] 
+
+        output_df = pd.DataFrame(self.complete_rows, columns=self._columns)
+        output_df.replace(to_replace = 0, value = '', inplace=True)
+        return output_df
 
     def formatTide(self):
         df = pd.read_csv(self._filename)
@@ -88,14 +133,11 @@ class FormatTide():
 
         for i, row in df_sorted.iterrows(): #For each row, need i to get row
             current_gene = row['Gene']
-            self._resetCurretRow(current_gene)
-            cohort = row['Cohort']
-            self._tcgaFlip(cohort)
+            self._resetCurretRow(current_gene) #Reset all vars for next row
 
+            cohort = row['Cohort']
+            cohort = self._tcgaFlip(cohort)
             self._getValues(cohort, row)
             self.genes.append(current_gene)
 
-        output_df = pd.DataFrame(self.complete_rows, columns=self._columns)
-        output_df.replace(to_replace = 0, value = '', inplace=True)
-        return output_df
-
+        return self._outputDf()
--- Tide/tideMain.py
+++ Tide/tideMain.py
@@ -1,96 +1,169 @@
 import os
 import shutil
+import argparse
 import pandas as pd
 from pathlib import Path
 from tideFormat import *
 from tideQuery import RegisterTide
 from tideQuery import QueryTide
 
-#DIRECTORIES - DO NOT CHANGE
-MAIN_DIR = str(Path(__file__).resolve().parent)
-DOWNLOADS = MAIN_DIR + "/downloads" 
-DOWNLOAD_DIR = str(Path.home() / "Downloads") 
-# CHANGEME for different dataset filtering
-COLUMNS = ['Gene', "GSE26712@PRECOG", "GSE13876@PRECOG", "GSE3149@PRECOG", 
-            "GSE9899@PRECOG", "GSE17260@PRECOG", 
-            "GSE17260", "GSE49997", "GSE32062", "GSE26712", 
-            'TCGA1', 'TCGA2','CAF FAP', 'MDSC', 'TAM M2']
-EXCLUSION = True
+class TideUtilities():
+    def __init__(self, main_dir, download_dir, downloads, exclusion, genes) -> None:
+        self.main_dir = main_dir
+        self.download_dir = download_dir
+        self.downloads = downloads
+        self.exclusion = exclusion
+        self.genes = genes
 
-def createDownloadsDir():
-    if not os.path.exists(DOWNLOADS):
-        os.makedirs(DOWNLOADS)    
+    def checkFileExists(self, filename):
+        return filename in os.listdir(self.downloads)
 
-def checkFileExists(filename):
-    return filename in os.listdir(DOWNLOADS)
+    def renameFile(self, gene, exclusion):
+        exclusion_str = "_exclusion" if exclusion else ""
+        old_name = f"{self.download_dir}/{gene + exclusion_str}.csv"
+        new_name = f"{self.downloads}/{gene + exclusion_str}.csv"
+        shutil.move(old_name, new_name)
 
-def renameFile(gene, exclusion):
-    exclusion_str = "_exclusion" if exclusion else ""
-    old_name = f"{DOWNLOAD_DIR}/{gene + exclusion_str}.csv"
-    new_name = f"{DOWNLOADS}/{gene + exclusion_str}.csv"
-    shutil.move(old_name, new_name)
+    @staticmethod
+    def createDownloadsDir(downloads):
+        if not os.path.exists(downloads):
+            os.makedirs(downloads)   
 
-def importFile(filename):
-    with open(filename, 'r') as file:
-        data = file.read()
-    # Split the data based on commas and store the strings in a list
-    string_list = [string.strip() for string in data.split(',')]
-    # Print the list of strings
-    file.close()
-    return string_list
+    @staticmethod
+    def importFile(filename):
+        with open(filename, 'r') as file:
+            data = file.read()
+        # Split the data based on commas and store the strings in a list
+        string_list = [string.strip() for string in data.split(',')]
+        # Print the list of strings
+        file.close()
+        return string_list
 
-def writeErrors(errors: list):
-    with open(f"{MAIN_DIR}/tideErrors.txt", 'w') as file:
-            file.truncate(0)
-            for error in errors:
-                file.write(f'{error},')
-    file.close()
+    @staticmethod
+    def writeErrors(filename, errors: list):
+        with open(filename, 'w') as file:
+                file.truncate(0)
+                for error in errors:
+                    file.write(f'{error},')
+        file.close()
 
-def queryData(genes):
-    errors = []
-    register = RegisterTide(DOWNLOAD_DIR)
-    email = register.registerTide()
-    for index, gene in enumerate(genes, 1): #Start enumerate at 1 so doesn't register at start
-        if checkFileExists(f"{gene}.csv") \
-            and checkFileExists(f"{gene}_exclusion.csv"):
-            continue
-        if index % 400 == 0: #400 seems reasonable till new account
-            email = register.registerTide()
-        try:
-            QueryTide(DOWNLOAD_DIR, gene, email).tideQuery()
-            renameFile(gene, False) #For the expression download file
-            renameFile(gene, True) #For the exclusion download file
-            print(f"{gene} success query")
-        except Exception as error:
-            errors.append(gene)
-            print(f"{gene} failed query because: \n {error}")
-    return errors
+class TideQuery(TideUtilities):
+    def __init__(self, main_dir, download_dir, downloads, exclusion, genes):
+        super().__init__(main_dir, download_dir, downloads, exclusion, genes)
+        self.errors = []
 
-def combineDownloads(genes):
-    dataframes = []
-    errors = []
-    for gene in genes:
-        try:
-            dataframes.append(FilerTide(DOWNLOADS, gene, EXCLUSION).filterData())
-            concat_df = pd.concat(dataframes, ignore_index=True)
-            concat_df.to_csv(f"{MAIN_DIR}/tide_output.csv", index=False)
-            print(f"{gene} success combine")
-        except FileNotFoundError as error:
-            errors.append(gene)
-            print(f"{gene} failed combine because: \n {error}")
-    FormatTide(f"{MAIN_DIR}/tide_output.csv", COLUMNS, EXCLUSION).formatTide()\
-        .to_csv(f"{MAIN_DIR}/tide_output_formatted.csv")
-    return errors
+    def register(self):
+        self.register = RegisterTide(self.download_dir) # type: ignore
+        self.email = self.register.registerTide()
+        
+    def ifFileExists(self, gene):
+        if self.exclusion:
+            return self.checkFileExists(f"{gene}.csv") \
+                and self.checkFileExists(f"{gene}_exclusion.csv")
+        else:
+            return self.checkFileExists(f"{gene}.csv")
+        
+    def renameFile(self, gene):
+        super().renameFile(gene, False)
+        if self.exclusion:
+            super().renameFile(gene, True)
+
+    def queryData(self):
+        self.register()
+        for index, gene in enumerate(self.genes, 1): #Start enumerate at 1 so doesn't register at start
+            if self.ifFileExists(gene): #Check if file already in downloads, don't need to query.
+                continue
+            if index % 400 == 0: #400 seems reasonable till new account
+                self.email = self.register.registerTide()
+            try:
+                QueryTide(self.download_dir, gene, self.email).tideQuery()
+                self.renameFile(gene)
+                print(f"{gene} success query")
+            except Exception as error:
+                self.errors.append(gene)
+                print(f"{gene} failed query because: \n {error}")
+        return self.errors
+    
+class TideFormat(TideUtilities):
+    def __init__(self, main_dir, download_dir, downloads, exclusion, genes, cancers) -> None:
+        super().__init__(main_dir, download_dir, downloads, exclusion, genes)
+        self.cancer = cancers
+        self.dataframes = []
+        self.errors = []
+
+    def combineDownloads(self):
+        for gene in self.genes:
+            try:
+                self.dataframes.append(
+                    FilerTide(self.downloads, gene, self.exclusion, self.cancer).filterData())
+                concat_df = pd.concat(self.dataframes, ignore_index=True)
+                concat_df.to_csv(f"{self.main_dir}/tide_output.csv", index=False)
+                print(f"{gene} success combine")
+            except FileNotFoundError as error:
+                self.errors.append(gene)
+                print(f"{gene} failed combine because: \n {error}")
+    
+    def downloadFormat(self):
+        self.combineDownloads()
+
+        columns = self.importFile(f"{self.main_dir}/tideCohorts.txt")
+        FormatTide(f"{self.main_dir}/tide_output.csv", columns, self.exclusion).formatTide()\
+            .to_csv(f"{self.main_dir}/tide_output_formatted.csv", index=False)
+        
+        return self.errors
+
+class Tide():
+    def __init__(self, main_dir, download_dir, downloads, exclusion, query, cancers) -> None:
+        self.main_dir = main_dir
+        self.downloads = downloads
+        self.query = query
+        self.errors = []
+
+        genes = TideUtilities.importFile(f"{self.main_dir}/tideGenes.txt")
+        self.tideQuery = TideQuery(main_dir, download_dir, downloads, exclusion, genes)
+        self.tideFormat = TideFormat(main_dir, download_dir, downloads, exclusion, genes, cancers)
+
+    def tideMain(self):
+        TideUtilities.createDownloadsDir(self.downloads)
+        # if self.query:
+        #     self.errors.extend(self.tideQuery.queryData())
+        self.errors.extend(self.tideFormat.downloadFormat())
+        TideUtilities.writeErrors(f"{self.main_dir}/tideErrors.txt", self.errors)
+        print("Success All Genes Formatted")
+
+def argParser(main_dir, downloads):
+    parser = argparse.ArgumentParser(
+        description='Gets Tide T_Dysfunction from expression and exclusion data')
+
+    parser.add_argument("-o", "--outdir", default=main_dir, 
+                        help="Directory to where outputted files and csv download files should be stored" + f"\nDefault is {main_dir}")
+    
+    parser.add_argument("-d", "--downloads", default=downloads, 
+                        help="Change specifically where the csv files are downloaded to." 
+                        + f"\nDefault is {downloads}")
+
+    cancer_types = ["Head Neck", "Colorectal", "Lung", "Melanoma", "Lymphoma", "Ovary", "Leukemia",
+                    "Breast", "Glioblastoma", "Myeloma", "Sarcoma", "Liver", "Bladder", "Brain",	
+                    "Ovarian", "Esophageal", "Kidney"]
+    parser.add_argument("-c", "--cancer", choices=cancer_types, default=['Ovarian', 'Ovary'], 
+                        nargs='+', help="Cancer type to run Tide on. Default is: ['Ovarian', 'Ovary'].")
+    
+    parser.add_argument("-q", "--query", action='store_false', 
+                        help="Do NOT query Tide just combine and format downloaded genes.")
+    
+    parser.add_argument("-e", "--exclusion", action='store_false', 
+                        help="Do NOT download or format exclusion values from Tide.")
+    
+    return parser.parse_args()
 
 def main():
-    genes = importFile(f"{MAIN_DIR}/tideGenes.txt")
-    errors = [] #define errors so can combine downloads without querying and won't error
-    createDownloadsDir()
+    MAIN_DIR = str(Path(__file__).resolve().parent) #Directory of python script
+    DOWNLOADS = MAIN_DIR + "/Downloads"
+    DOWNLOAD_DIR = str(Path.home() / "Downloads") #System downloads path.
 
-    errors = queryData(genes)
-    errors.extend(combineDownloads(genes))
-    writeErrors(errors)
-    print("Success All Genes Formatted")
+    parser = argParser(MAIN_DIR, DOWNLOADS)
+    Tide(parser.outdir, DOWNLOAD_DIR, parser.downloads, 
+         parser.exclusion, parser.query, parser.cancer).tideMain()
 
 if __name__ == "__main__":
    main()
